e8ca3c6ac28bb4763cb8b532892dd223cf8a3540
Matias Tucci <matiastucci@gmail.com>
2017-03-04 21:34:59 -0300

    check multiple domains

--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
-const request = require('request');
-const emojis = require('emojis-list');
-const punycode = require('punycode');
+const request = require('request')
+const emojis = require('emojis-list')
+const punycode = require('punycode')
 
-const checkIfEmojiDomainIsAvailable = emoji => {
-  const url = 'http://samoanic.ws/whois.dhtml';
+const checkIfWSDomainIsAvailable = emoji => {
+  const url = 'http://samoanic.ws/whois.dhtml'
   const options = {
     url,
     form: {domain: `xn--${punycode.encode(emoji)}`, tld: 'WS'}
-  };
+  }
 
   request.post(options, (error, response, body) => {
     if (error) {
-      console.log(`Error checking ${emoji} .ws domain`);
-      return;
+      console.log(`Error checking ${emoji} .ws domain`)
+      return
     }
 
     if (body.toLowerCase().includes('congratulations')) {
-      console.log(`${emoji} .ws is available`);
+      console.log(`${emoji} .ws is available`)
     }
-  });
-};
+  })
+}
+
+const checkIfDomainsAreAvailable = emoji => {
+  const url = 'https://my.freenom.com/includes/domains/fn-available.php'
+  const options = {
+    url,
+    form: {domain: `xn--${punycode.encode(emoji)}`, tld: ''}
+  }
+
+  request.post(options, (error, response, body) => {
+    if (error) {
+      console.log(`Error checking ${emoji}  domain`)
+      return
+    }
+
+    const bodyObj = JSON.parse(body)
+
+    bodyObj.free_domains.forEach(domain => {
+      if (domain.status === 'AVAILABLE') {
+        console.log(`${emoji} ${domain.tld} is available`)
+      }
+    })
+  })
+}
 
 emojis.forEach(emoji => {
-  checkIfEmojiDomainIsAvailable(emoji);
+  checkIfDomainsAreAvailable(emoji)
+  checkIfWSDomainIsAvailable(emoji)
 })
